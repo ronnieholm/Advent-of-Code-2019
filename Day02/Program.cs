b01c20edd7445d615cdb5167a1c08fe54d71000d
Ronnie Holm <mail@bugfree.dk>
2019-12-02 20:09:00 +0100

    Day 02, part 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Diagnostics;
-using Memory = System.Collections.Generic.Dictionary<int, int>;
 
 namespace Day02
 {
@@ -11,6 +8,7 @@ namespace Day02
     {
         static void Main(string[] args)
         {
+            // Test
             var m1 = LoadMemory("1,9,10,3,2,3,11,0,99,30,40,50");
             var m2 = LoadMemory("3500,9,10,70,2,3,11,0,99,30,40,50");
             Execute(m1);
@@ -18,51 +16,73 @@ namespace Day02
 
             var input = File.ReadAllText("Input.txt");
             var memory = LoadMemory(input);
+
+            // Part 1
             memory[1] = 12;
             memory[2] = 2;
             Execute(memory);
             Debug.Assert(memory[0] == 3654868);
+
+            // Part 2
+            for (var i = 0; i <= 99; i++)
+            {
+                for (var j = 0; j <= 99; j++)
+                {
+                    memory = LoadMemory(input);
+                    memory[1] = i;
+                    memory[2] = j;                   
+                    Execute(memory);
+                    if (memory[0] == 19690720)
+                    {
+                        var nounVerb = $"{i}{j}";
+                        Console.WriteLine(nounVerb);
+                        Debug.Assert(nounVerb == "7014");
+                        break;
+                    }
+
+                }
+            }
         }       
 
-        static bool Compare(Memory m1, Memory m2)
+        static bool Compare(int[] m1, int[] m2)
         {
-            if (m1.Count != m2.Count)
+            if (m1.Length != m2.Length)
                 return false;
             
-            foreach (var kvp in m1)
-                if (m1[kvp.Key] != m2[kvp.Key])
+            for (var i = 0; i < m1.Length; i++)
+                if (m1[i] != m2[i])
                     return false;
 
             return true;
         }
 
-        static Memory LoadMemory(string s)
+        static int[] LoadMemory(string s)
         {
-            var memory = new Memory();
             var strings = s.Split(',');
-            var ints = new int[strings.Length];            
+            var memory = new int[strings.Length];
+
             for (var i = 0; i < strings.Length; i++)
                 memory[i] = int.Parse(strings[i]);
 
             return memory;
         }
 
-        static void Execute(Memory memory)
+        static void Execute(int[] memory)
         {
-            for (var i = 0; i < memory.Count; i += 4)
+            for (var i = 0; i < memory.Length; i += 4)
             {
                 var opcode = memory[i];
-                var input1 = memory[i + 1];
-                var input2 = memory[i + 2];
-                var output = memory[i + 3];
+                var inputAddress1 = memory[i + 1];
+                var inputAddress2 = memory[i + 2];
+                var outputAddress = memory[i + 3];
 
                 switch (opcode)
                 {
                     case 1:
-                        memory[output] = memory[input1] + memory[input2];
+                        memory[outputAddress] = memory[inputAddress1] + memory[inputAddress2];
                         break;
                     case 2:
-                        memory[output] = memory[input1] * memory[input2];
+                        memory[outputAddress] = memory[inputAddress1] * memory[inputAddress2];
                         break;
                     case 99:
                         return;
